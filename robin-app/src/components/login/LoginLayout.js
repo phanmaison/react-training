8c5a87b3a750b3b5d0faf90bd33d22ec32078c07
Robin <phanmaison@gmail.com>
2020-04-06 15:22:30 +0700

    Update login layout

--- robin-app/src/components/login/LoginLayout.js
+++ robin-app/src/components/login/LoginLayout.js
@@ -1,33 +1,34 @@
-import React from 'react';
+import React from "react";
 import {
-    BrowserRouter as Router,
-    Switch,
-    Route,
-    Link,
-    useParams,
-    useRouteMatch
-  } from "react-router-dom";
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link,
+  useParams,
+  useRouteMatch,
+} from "react-router-dom";
+import "./LoginLayout.css";
+import 'bootstrap/dist/css/bootstrap.css';
 
-export default class LoginLayout extends React.Component{
+export default class LoginLayout extends React.Component {
+  onLogin = () => {
+    console.log("onLogin");
+  };
 
-    onLogin = () => {
-        console.log("onLogin");
-    }
-
-    render() {
-        return (
-            <div>
-                <div className="App">
-                    Login page
-                    
-                <div style={{float: "right"}}>
-                  <Link to="/">Main Page</Link>
-                  </div>
-                </div>
-                <div className="container">
-                    <input type="text" /> <button onClick={this.onLogin}>Login</button>
-                </div>
-            </div>
-        );
-      }
+  render() {
+    return (
+      <div className="login-container">
+        <div style={{ float: "right" }}>
+          <Link to="/">Main Page</Link>
+        </div>
+        <div className="login-block">
+          <h3 className="center">Login</h3>
+          <hr />
+          <div className="form-inline">
+            <input type="text" className="form-control mr-2" placeholder="Enter username" /> <button className="btn btn-primary" onClick={this.onLogin}>Login</button>
+          </div>
+        </div>
+      </div>
+    );
+  }
 }
--- robin-app/src/components/main/MainLayout.js
+++ robin-app/src/components/main/MainLayout.js
@@ -1,35 +1,33 @@
-import React from 'react';
-import logo from '../../logo.svg';
-import '../../App.css';
-import { Link } from 'react-router-dom';
+import React from "react";
+import logo from "../../logo.svg";
+import "../../App.css";
+import { Link } from "react-router-dom";
 
-export default class MainLayout extends React.Component{
-    
-
-    render(){
-        return(
-            <div className="App">
-              <header>
-                Sample App
-                <div style={{float: "right"}}>
-                  <Link to="/login">Login</Link>
-                  </div>
-              </header>
-              <header className="App-header">
-                <img src={logo} className="App-logo" alt="logo" />
-                <p>
-                  Edit <code>src/App.js</code> and save to reload.
-                </p>
-                <a
-                  className="App-link"
-                  href="https://reactjs.org"
-                  target="_blank"
-                  rel="noopener noreferrer"
-                >
-                  Test React
-                </a>
-              </header>
-            </div>
-        );
-    }
+export default class MainLayout extends React.Component {
+  render() {
+    return (
+      <div className="App">
+        <header>
+          Sample App
+          <div style={{ float: "right" }}>
+            <Link to="/login">Login</Link>
+          </div>
+        </header>
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <p>
+            Edit <code>src/App.js</code> and save to reload.
+          </p>
+          <a
+            className="App-link"
+            href="https://reactjs.org"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Test React
+          </a>
+        </header>
+      </div>
+    );
+  }
 }
