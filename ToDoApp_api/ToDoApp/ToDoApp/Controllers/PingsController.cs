016b680e8152a8c2d79553f93916ed34a9936818
MyLinh97 <tranlinh0962@gmail.com>
2020-04-08 15:00:24 +0700

    convert net.core 2.2

---
+++ ToDoApp_api/ToDoApp/ToDoApp/Controllers/PingsController.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+using ToDoApp.Domain.Entity;
+using ToDoApp.Model;
+using ToDoApp.Service.Interfaces;
+
+namespace ToDoApp.Controllers
+{
+    [Route("api/Pings")]
+    [ApiController]
+    public class PingsController : ControllerBase
+    {
+        public PingsController()
+        {
+        }
+
+        [HttpGet]
+        [Route("")]
+        public async Task<ActionResult> Index()
+        {
+            return new ObjectResult(new
+            {
+                Code = 1,
+                Data = new
+                {
+                    time = "2020/04/08 12:00",
+                },
+                Message = "Online"
+            });
+        }
+
+    }
+}
